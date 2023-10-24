68ba0bee644500e3f8da7e90e2884410dd4c7834
timcalaguas <timcalaguas@gmail.com>
2023-10-24 22:15:54 +0800

    Changed version for next auth

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { firestore } from "../../../../firebase-config";
+
 export default async function auth(req, res) {
   return await NextAuth(req, res, {
     providers: [
