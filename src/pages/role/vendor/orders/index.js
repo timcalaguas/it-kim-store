bc8a85d47e9eafab16deab40cbc27322cce7d221
timcalaguas <timcalaguas@gmail.com>
2023-12-03 18:42:54 +0800

    Fix build error

--- src/pages/role/vendor/orders/index.js
+++ src/pages/role/vendor/orders/index.js
@@ -1,8 +1,9 @@
 import AdminLayout from "@/components/AdminLayout";
-import { firestore } from "../../../../../../firebase-config";
+import { firestore } from "../../../../../firebase-config";
+import { getSession } from "next-auth/react";
 import { FiHome, FiTrendingUp, FiCompass, FiStar } from "react-icons/fi";
 import {
-  AiFillShop,
+  AiTwotoneEdit,
   AiFillDelete,
   AiFillEye,
   AiFillCheckCircle,
@@ -21,6 +22,13 @@ import {
   Heading,
   HStack,
   Image,
+  Link,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -35,46 +43,29 @@ import {
   Divider,
   Avatar,
   VStack,
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogContent,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  AlertDialogOverlay,
-  Tag,
-  TagLabel,
 } from "@chakra-ui/react";
 import { useEffect, useState, useRef } from "react";
-import { MdDeliveryDining, MdPerson } from "react-icons/md";
-
 import moment from "moment/moment";
+import getVendorOrders from "@/hooks/vendors/getVendorOrders";
 import { withSessionSsr } from "@/lib/withSession";
-import getUsers from "@/hooks/getUsers";
+import axios from "axios";
+import getBodyForEmail from "@/hooks/getBodyForEmail";
 
-import { BiSolidShoppingBag } from "react-icons/bi";
 import { FaPesoSign } from "react-icons/fa6";
-import { FaStar } from "react-icons/fa";
-import getWorkerDetails from "@/hooks/admin/getVendorDetails";
-import { AiTwotoneMail, AiTwotoneHome, AiTwotonePhone } from "react-icons/ai";
-
+import { AiFillShopping } from "react-icons/ai";
+import { BsFillCartCheckFill } from "react-icons/bs";
 const LinkItems = [
-  { name: "Dashboard", icon: FiHome, link: "/role/admin" },
-  { name: "Vendors", icon: AiFillShop, link: "/role/admin/vendors" },
-  { name: "Couriers", icon: MdDeliveryDining, link: "/role/admin/couriers" },
-  { name: "Customers", icon: MdPerson, link: "/role/admin/customers" },
-  { name: "Sales Report", icon: FaPesoSign, link: "/role/admin/sales-report" },
+  { name: "Dashboard", icon: FiHome, link: "/role/vendor" },
+  { name: "Products", icon: AiFillShopping, link: "/role/vendor/products" },
+  { name: "Orders", icon: BsFillCartCheckFill, link: "/role/vendor/orders" },
+  { name: "Sales Report", icon: FaPesoSign, link: "/role/vendor/sales-report" },
 ];
 
-const Vendors = ({ vendorDocs, user }) => {
+const Orders = ({ orderDocs, user }) => {
   const toast = useToast();
 
-  const [vendor, setVendor] = useState(vendorDocs);
-  const [selectedId, setSelectedId] = useState("");
-  const [process, setProcess] = useState("accept");
+  const [orders, setOrders] = useState(orderDocs);
+  const [process, setProcess] = useState("");
   const [selectedItem, setSelectedItem] = useState([]);
   const [processLoading, setProcessLoading] = useState(false);
 
@@ -87,134 +78,64 @@ const Vendors = ({ vendorDocs, user }) => {
   } = useDisclosure();
   const cancelRef = useRef();
 
-  const openProcessDialog = (vendor, process) => {
-    setSelectedItem(vendor);
+  const openDeleteDialog = (order, process) => {
+    setSelectedItem(order);
 
     setProcess(process);
     onOpen();
   };
 
-  const openModal = (vendor) => {
-    setSelectedItem(vendor);
+  const openModal = (order) => {
+    setSelectedItem(order);
     itemOnOpen();
   };
 
-  const processVendor = async () => {
+  const processOrder = async () => {
     setProcessLoading(true);
-    const indexOfObjectToUpdate = vendors.findIndex(
+    const indexOfObjectToUpdate = orders.findIndex(
       (obj) => obj.id === selectedItem.id
     );
-    let status = process == "accept" ? "approved" : "blocked";
+
+    let status = process == "accept" ? "order-accepted" : "order-declined";
+
+    const bodyForEmail = await getBodyForEmail(
+      status,
+      selectedItem.customer,
+      user
+    );
+
+    const response = await axios.post("/api/send-mail", bodyForEmail);
+
     const processResponse = await firestore
-      .collection("users")
+      .collection("orders")
       .doc(selectedItem.id)
       .update({ status: status });
     selectedItem.status = status;
     setProcessLoading(false);
-    vendors[indexOfObjectToUpdate] = selectedItem;
-
+    onClose();
+    orders[indexOfObjectToUpdate] = selectedItem;
     toast({
-      title: process == "accept" ? "Approved" : "Blocked",
+      title: process == "accept" ? "Order Accepted" : "Order Declined",
       description:
         process == "accept"
-          ? "The vendor is now approved."
-          : "The vendor is now blocked.",
+          ? "The order is now accepted."
+          : "The order is now declined.",
       status: "success",
       duration: 9000,
       isClosable: true,
     });
-    onClose();
   };
 
   return (
     <>
       <AdminLayout
-        metaTitle={"Admin - Vendors"}
-        pageName={"IT Kim - Admin"}
+        metaTitle={"Vendor - Orders"}
+        pageName={"IT Kim - Vendor"}
         user={user}
         LinkItems={LinkItems}
       >
-        <Card mb={"24px"}>
-          <CardBody>
-            <HStack
-              flexWrap={"wrap"}
-              alignItems={"start"}
-              justifyContent={"space-between"}
-            >
-              <HStack gap={"24px"} flexWrap={"wrap"}>
-                <VStack>
-                  <Avatar src={vendor.storeLogo} boxSize={"150px"} />
-                  <Tag
-                    size="md"
-                    colorScheme={
-                      user.status === "approved"
-                        ? "green"
-                        : user.status === "pending"
-                        ? "orange"
-                        : "red"
-                    }
-                    borderRadius="full"
-                    marginBlock={"12px"}
-                  >
-                    {user.status === "approved" ? (
-                      <TagLabel textTransform={"uppercase"}>
-                        {user.status}
-                      </TagLabel>
-                    ) : (
-                      <TagLabel textTransform={"uppercase"}>Pending</TagLabel>
-                    )}
-                  </Tag>
-                </VStack>
-                <Box>
-                  <Text fontSize={"24px"} fontWeight={"600"}>
-                    {vendor.storeName != "" ? vendor.storeName : vendor.name}
-                  </Text>
-                  <HStack>
-                    <AiTwotoneMail />
-                    <Text>{vendor.email}</Text>
-                  </HStack>
-                  <HStack>
-                    <AiTwotoneHome />
-                    <Text>
-                      {vendor.addresses?.length > 0
-                        ? `${vendor.addresses[0].address.no} ${vendor.addresses[0].address.street} ${vendor.addresses[0].address.barangay} ${vendor.addresses[0].address.city}`
-                        : ""}
-                    </Text>
-                  </HStack>
-                  <HStack>
-                    <AiTwotonePhone />
-                    <Text>{vendor.addresses[0].contactNumber}</Text>
-                  </HStack>
-
-                  <Button mt={"24px"} onClick={() => openModal(vendor)}>
-                    View Requirement
-                  </Button>
-                </Box>
-              </HStack>
-              <HStack>
-                {vendor.status != "approved" && (
-                  <Button
-                    colorScheme="blue"
-                    onClick={() => openProcessDialog(vendor, "accept")}
-                  >
-                    Approve
-                  </Button>
-                )}
-                {vendor.status != "blocked" && (
-                  <Button
-                    colorScheme="red"
-                    onClick={() => openProcessDialog(vendor, "block")}
-                  >
-                    Block
-                  </Button>
-                )}
-              </HStack>
-            </HStack>
-          </CardBody>
-        </Card>
-
         <HStack alignItems={"center"} justifyContent={"space-between"} mb={6}>
-          <Heading>Ratings</Heading>
+          <Heading>List of Orders</Heading>
         </HStack>
 
         <TableContainer
@@ -222,26 +143,112 @@ const Vendors = ({ vendorDocs, user }) => {
           p={{ base: 2, md: 5 }}
           borderRadius={6}
         >
-          {vendor.ratings.length > 0 ? (
+          {orders.length > 0 ? (
             <Table variant="simple">
               <Thead>
                 <Tr>
-                  <Th>Name</Th>
-                  <Th>Rating</Th>
-                  <Th>Comment</Th>
+                  <Th>Order ID</Th>
+                  <Th>Date</Th>
+                  <Th>Customer</Th>
+                  <Th>Status</Th>
+                  <Th>Actions</Th>
                 </Tr>
               </Thead>
               <Tbody>
-                {vendor.ratings.map((rating) => (
+                {orders.map((order) => (
                   <Tr>
-                    <Td>{rating.commentor}</Td>
-                    <Td>
-                      <HStack>
-                        <Text>{rating.starRating} </Text>
-                        <FaStar color="gold" />
-                      </HStack>
+                    <Td>{order.id}</Td>
+                    <Td>{moment(new Date()).format("MM/DD/YYYY")}</Td>
+                    <Td>{order.customer.name}</Td>
+                    <Td textTransform={"uppercase"}>
+                      {order.status == "cancelled" ? (
+                        <Popover>
+                          <PopoverTrigger>
+                            <Badge
+                              display={"flex"}
+                              w={"fit-content"}
+                              alignItems={"center"}
+                              gap={"6px"}
+                              cursor={"pointer"}
+                              colorScheme={
+                                order.status == "received" ||
+                                order.status == "delivered"
+                                  ? "green"
+                                  : order.status == "order-placed" ||
+                                    order.status == "order-accepted" ||
+                                    order.status == "in-transit"
+                                  ? "blue"
+                                  : "red"
+                              }
+                            >
+                              {order.status}{" "}
+                              <FaQuestionCircle fontSize={"10px"} />
+                            </Badge>
+                          </PopoverTrigger>
+                          <PopoverContent>
+                            <PopoverArrow />
+                            <PopoverCloseButton />
+                            <PopoverHeader>Cancel Reason</PopoverHeader>
+                            <PopoverBody>{order.cancelReason}</PopoverBody>
+                          </PopoverContent>
+                        </Popover>
+                      ) : (
+                        <Badge
+                          colorScheme={
+                            order.status == "received" ||
+                            order.status == "delivered"
+                              ? "green"
+                              : order.status == "order-placed" ||
+                                order.status == "order-accepted" ||
+                                order.status == "in-transit"
+                              ? "blue"
+                              : "red"
+                          }
+                        >
+                          {order.status}
+                        </Badge>
+                      )}
                     </Td>
-                    <Td>{rating.comment}</Td>
+                    <Td>
+                      <Stack direction="row" spacing={2}>
+                        <Button
+                          size={"sm"}
+                          colorScheme="orange"
+                          variant={"outline"}
+                          leftIcon={<AiFillEye />}
+                          onClick={() => openModal(order)}
+                        >
+                          View Details
+                        </Button>
+                        {order.status == "order-placed" &&
+                          order.status != "cancelled" && (
+                            <>
+                              <Button
+                                leftIcon={<AiFillCheckCircle />}
+                                colorScheme="blue"
+                                variant="outline"
+                                size={"sm"}
+                                onClick={() =>
+                                  openDeleteDialog(order, "accept")
+                                }
+                              >
+                                Accept
+                              </Button>
+                              <Button
+                                leftIcon={<AiFillDelete />}
+                                colorScheme="red"
+                                size={"sm"}
+                                variant="outline"
+                                onClick={() =>
+                                  openDeleteDialog(order, "decline")
+                                }
+                              >
+                                Decline
+                              </Button>
+                            </>
+                          )}
+                      </Stack>
+                    </Td>
                   </Tr>
                 ))}
               </Tbody>
@@ -253,7 +260,7 @@ const Vendors = ({ vendorDocs, user }) => {
               placeItems={"center"}
               textAlign={"center"}
             >
-              <Heading>No ratings yet</Heading>
+              <Heading>No orders yet</Heading>
             </Box>
           )}
         </TableContainer>
@@ -266,13 +273,13 @@ const Vendors = ({ vendorDocs, user }) => {
         <AlertDialogOverlay>
           <AlertDialogContent>
             <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              {process == "accept" ? "Approve Vendor" : "Block Vendor"}
+              {process == "accept" ? "Accept Order" : "Decline Order"}
             </AlertDialogHeader>
 
             <AlertDialogBody>
               {process == "accept"
-                ? "Are you sure you want to approve this vendor? This will allow the vendor to publish their products."
-                : "Are you sure you want to block this vendor? They will no longer be allowed to use their store and sell products."}
+                ? "Are you sure you want to accept this order? After accepting this order will be available to all couriers."
+                : "Are you sure you want to decline this order? After declining the order will be cancelled and the customer will be notified."}
             </AlertDialogBody>
 
             <AlertDialogFooter>
@@ -281,30 +288,84 @@ const Vendors = ({ vendorDocs, user }) => {
               </Button>
               <Button
                 colorScheme={process == "accept" ? "blue" : "red"}
-                onClick={() => processVendor()}
+                onClick={() => processOrder()}
                 ml={3}
                 isLoading={processLoading}
               >
-                {process == "accept" ? "Approve" : "Block"}
+                {process == "accept" ? "Accept" : "Decline"}
               </Button>
             </AlertDialogFooter>
           </AlertDialogContent>
         </AlertDialogOverlay>
       </AlertDialog>
 
-      <Modal isOpen={itemIsOpen} onClose={itemOnClose} size={"xl"}>
+      <Modal isOpen={itemIsOpen} onClose={itemOnClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Requirement</ModalHeader>
+          <ModalHeader>Details</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Image
-              src={
-                selectedItem.requirement
-                  ? selectedItem.requirement
-                  : "https://placehold.co/700x400"
-              }
-            />
+            <HStack mb={"12px"}>
+              <Avatar src={selectedItem.customer?.picture} />
+              <Box>
+                <Text>{selectedItem.customer?.name}</Text>
+                <Text>{selectedItem.customer?.email}</Text>
+              </Box>
+            </HStack>
+            <Text mb={"12px"}>
+              <Text fontWeight={"500"}>Address:</Text>{" "}
+              {selectedItem.customer?.address &&
+                `${selectedItem.customer.address.address.no} ${selectedItem.customer.address.address.street} ${selectedItem.customer.address.address.barangay} ${selectedItem.customer.address.address.city}`}
+            </Text>
+            <Text mb={"12px"}>
+              <Text fontWeight={"500"}>Contact Number:</Text>{" "}
+              {selectedItem.customer?.address &&
+                selectedItem.customer.address.contactNumber}{" "}
+            </Text>
+            <Divider marginBlock={"6px"} />
+            <Box>
+              <Text fontWeight={"600"}>Items</Text>
+              {selectedItem.items &&
+                selectedItem.items.map((item) => (
+                  <HStack
+                    paddingBlock={4}
+                    alignItems={"center"}
+                    justifyContent={"space-between"}
+                    w={"100%"}
+                    flexWrap={"wrap"}
+                    key={item.id}
+                  >
+                    <HStack gap={4} w={"50%"}>
+                      <Image
+                        src={item.image}
+                        boxSize={"50px"}
+                        borderRadius={"lg"}
+                      />
+                      <Box>
+                        <Text fontSize={"md"} fontWeight={"medium"}>
+                          {item.productName}
+                        </Text>
+                        <Text fontSize={"md"}>{item.discountedPrice}</Text>
+                      </Box>
+                    </HStack>
+                    <HStack>
+                      <Text fontSize={"md"}>x {item.quantity}</Text>
+                    </HStack>
+                  </HStack>
+                ))}
+            </Box>
+            <VStack alignItems={"end"}>
+              <Text>
+                <b>Subtotal:</b> {selectedItem.subtotal}
+              </Text>
+              <Text>
+                <b>Shipping Fee:</b>{" "}
+                {selectedItem.total - selectedItem.subtotal}
+              </Text>
+              <Text>
+                <b>Total:</b> {selectedItem.total}
+              </Text>
+            </VStack>
           </ModalBody>
         </ModalContent>
       </Modal>
@@ -312,31 +373,29 @@ const Vendors = ({ vendorDocs, user }) => {
   );
 };
 
-export default Vendors;
+export default Orders;
 
-export const getServerSideProps = withSessionSsr(async (context) => {
-  const { req } = context;
-  const { id } = context.params;
-  const user = req.session.user;
+export const getServerSideProps = withSessionSsr(async ({ req, res }) => {
+  const user = req.session.user ? req.session.user : null;
 
   if (!user) {
     return {
       redirect: {
         permanent: false,
-        destination: "/role/admin/auth/login",
+        destination: "/role/vendor/auth/login",
       },
     };
   }
 
-  if (user.role != "admin") {
+  if (user.role != "vendor") {
     return {
       notFound: true,
     };
   }
 
-  const vendorDocs = await getWorkerDetails(id);
+  const orderDocs = await getVendorOrders(user.docId);
 
   return {
-    props: { vendorDocs, user },
+    props: { orderDocs, user },
   };
 });
