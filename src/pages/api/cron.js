c3ea810213a1821157955cf2ee7b38a392982423
timcalaguas <timcalaguas@gmail.com>
2023-12-03 09:34:08 +0800

    Adding CRON Again

--- src/pages/api/cron.js
+++ src/pages/api/cron.js
@@ -1,4 +1,9 @@
 export default function handler(request, response) {
+  if (
+    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return res.status(401).end("Unauthorized");
+  }
   console.log("123");
   console.log("Checking Vercel Cron");
   return response.json({ success: true });
