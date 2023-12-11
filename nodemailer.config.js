cb683dbb9ad1d4533de51296f60c5fb333624af7
Timothy Franco Calaguas <41162138+timcalaguas@users.noreply.github.com>
2023-12-11 22:56:20 +0800

    Update nodemailer.config.js

--- nodemailer.config.js
+++ nodemailer.config.js
@@ -3,8 +3,8 @@ module.exports = {
   port: 587,
   secureConnection: false,
   auth: {
-    user: "itkim.store@gmail.com",
-    pass: "xsmtpsib-bdefc68e1e5d067d329079d4599b8b48af85241a79c38b06dea8a8f393d56356-0jTM4ACqYzVvBFnp",
+    user: "itkimsweetshop@gmail.com",
+    pass: "Xr2jNTdIgLD0EBAS",
   },
   tls: {
     ciphers: "SSLv3",
